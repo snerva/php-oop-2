6988a943ff1c16405965eba0ade6ed64dbcd0c78
snerva <selenenerva@live.it>
2022-12-12 15:44:34 +0100

    classes

--- index.php
+++ index.php
@@ -9,3 +9,37 @@ BONUS (Opzionale):
 Il cliente potrà sia comprare i prodotti come ospite, senza doversi registrarsi nello store, oppure può iscriversi e creare un account per ricevere cosi il 20% di sconto.
 Il cliente effettua il pagamento dei prodotti nel carrello con la carta di credito, che non deve essere scaduta.
 */
+
+class Product
+{
+    public $name;
+    public $code;
+    public $price;
+    public $category;
+    public $image;
+
+    public function __construct($name, $code, $price, Category $category, $image)
+    {
+        $this->name = $name;
+        $this->code = $code;
+        $this->price = $price;
+        $this->category = $category;
+        $this->image =  $image;
+    }
+}
+
+class Category
+{
+}
+
+class Food extends Product
+{
+}
+
+class Toy extends Product
+{
+}
+
+class PetHouse extends Product
+{
+}
